e489250089dae49e49382155b07dcdf51c41dd51
vidhubirru <132962796+vidhubirru@users.noreply.github.com>
2024-07-17 14:40:40 -0700

    Made it so the inputs store the data.
    Box labeled json-output on the bottom is to visualize the json information. Needs to be removed before finalizing.

--- client/src/components/BuildingInfo.js
+++ client/src/components/BuildingInfo.js
@@ -1,214 +1,236 @@
-import React, { useState } from "react";
-import {
-  Grid,
-  TextField,
-  Button,
-  Typography,
-  Box,
-  Checkbox,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-} from "@mui/material";
+import React, { useState } from 'react';
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, Input, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 
-export default function BuildingInfo() {
-  const [buildingInfo, setBuildingInfo] = useState({
-    name: "",
-    buildingType: "",
-    addressType: "zipCode",
-    zipCode: "",
-    city: "",
-    state: "",
-    fullAddress: "",
+function BuildingInfo() {
+  const [buildingName, setBuildingName] = useState('');
+  const [buildingAddress, setBuildingAddress] = useState('');
+  const [buildingCity, setBuildingCity] = useState('');
+  const [buildingState, setBuildingState] = useState('');
+  const [buildingZipCode, setBuildingZipCode] = useState('');
+  const [buildingCountry, setBuildingCountry] = useState('');
+  const [useZipCodeOnly, setUseZipCodeOnly] = useState(false);
+  const [zipCodeOnly, setZipCodeOnly] = useState('');
+  const [buildingType, setBuildingType] = useState('');
+  const [hasSolar, setHasSolar] = useState(false);
+  const [hasBatteryStorage, setHasBatteryStorage] = useState(false);
+  const [hasEVSE, setHasEVSE] = useState(false);
+  const [electricalServiceType, setElectricalServiceType] = useState('');
+  const [availableCircuits, setAvailableCircuits] = useState('');
+  const [electricalCapacity, setElectricalCapacity] = useState('');
+  const [transformerAndDistributionEquipment, setTransformerAndDistributionEquipment] = useState({
+    electricalCapacity: '',
+    voltage: '',
   });
 
-  const [distributedEnergySource, setDistributedEnergySource] = useState({
-    onsiteDER: false,
-    desType: "",
-    capacity: "",
-  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let buildingLocation;
+    if (useZipCodeOnly) {
+      buildingLocation = {
+        zip_code: zipCodeOnly,
+      };
+    } else {
+      buildingLocation = {
+        address: buildingAddress,
+        city: buildingCity,
+        state: buildingState,
+        zip_code: buildingZipCode,
+        country: buildingCountry,
+      };
+    }
 
-  const [electricVehicleChargingStations, setElectricVehicleChargingStations] =
-    useState({
-      level: "",
-      portType: "",
-      maxChargingPerPort: "",
-      number: "",
-    });
+    const buildingData = {
+      building_name: buildingName,
+      building_location: buildingLocation,
+      building_type: buildingType,
+      has_solar: hasSolar,
+      has_battery_storage: hasBatteryStorage,
+      has_evse: hasEVSE,
+      electrical_service_type: electricalServiceType,
+      electrical_panel: {
+        available_circuits: availableCircuits,
+        electrical_capacity: electricalCapacity,
+      },
+      transformer_and_distribution_equipment: transformerAndDistributionEquipment,
+    };
 
-  const handleBuildingInfoChange = (event) => {
-    const { name, value } = event.target;
-    setBuildingInfo({ ...buildingInfo, [name]: value });
-  };
-
-  const handleDistributedEnergySourceChange = (event) => {
-    const { name, value } = event.target;
-    setDistributedEnergySource({ ...distributedEnergySource, [name]: value });
-  };
-
-  const handleElectricVehicleChargingStationsChange = (event) => {
-    const { name, value } = event.target;
-    setElectricVehicleChargingStations({
-      ...electricVehicleChargingStations,
-      [name]: value,
-    });
+    const json = JSON.stringify(buildingData, null, 2);
+    console.log(json); 
+    document.getElementById('json-output').innerText = json; // display the JSON in the HTML element
   };
 
   return (
-    <Grid container spacing={2} style={{ marginTop: 0, marginLeft: 10 }}>
-      <Typography variant="h4" gutterBottom>
-        BUILDING/SITE INFO
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h6" gutterBottom>
-            Building Information
-          </Typography>
-          <TextField
-            label="Name"
-            fullWidth
-            name="name"
-            value={buildingInfo.name}
-            onChange={handleBuildingInfoChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Building Type"
-            fullWidth
-            name="buildingType"
-            value={buildingInfo.buildingType}
-            onChange={handleBuildingInfoChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h6" gutterBottom>
-            Address
-          </Typography>
-          <RadioGroup
-            name="addressType"
-            value={buildingInfo.addressType}
-            onChange={handleBuildingInfoChange}
-          >
-            <FormControlLabel
-              value="zipCode"
-              control={<Radio />}
-              label="Zip Code"
+    <Box sx={{ maxWidth: 800, mx: 'auto', p: 2 }}>
+      <Typography variant='h1'>Building INFO </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Building Name"
+              value={buildingName}
+              onChange={(e) => setBuildingName(e.target.value)}
+              fullWidth
             />
+          </Grid>
+          <Grid item xs={12}>
             <FormControlLabel
-              value="fullAddress"
-              control={<Radio />}
-              label="Full Address"
-            />
-          </RadioGroup>
-          {buildingInfo.addressType === "zipCode" ? (
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <TextField
-                  label="Zip Code"
-                  fullWidth
-                  name="zipCode"
-                  value={buildingInfo.zipCode}
-                  onChange={handleBuildingInfoChange}
+              control={
+                <Checkbox
+                  checked={useZipCodeOnly}
+                  onChange={(e) => setUseZipCodeOnly(e.target.checked)}
                 />
-              </Grid>
+              }
+              label="Use Zip Code Only"
+            />
+          </Grid>
+          {useZipCodeOnly? (
+            <Grid item xs={12}>
+              <TextField
+                label="Zip Code"
+                value={zipCodeOnly}
+                onChange={(e) => setZipCodeOnly(e.target.value)}
+                fullWidth
+              />
             </Grid>
           ) : (
-            <TextField
-              label="Full Address"
-              fullWidth
-              name="fullAddress"
-              value={buildingInfo.fullAddress}
-              onChange={handleBuildingInfoChange}
+            <React.Fragment>
+              <Grid item xs={12}>
+                <TextField
+                  label="Address"
+                  value={buildingAddress}
+                  onChange={(e) => setBuildingAddress(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="City"
+                  value={buildingCity}
+                  onChange={(e) => setBuildingCity(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="State"
+                  value={buildingState}
+                  onChange={(e) => setBuildingState(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Zip Code"
+                  value={buildingZipCode}
+                  onChange={(e) => setBuildingZipCode(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Country"
+                  value={buildingCountry}
+                  onChange={(e) => setBuildingCountry(e.target.value)}
+                  fullWidth
+                />
+              </Grid>
+            </React.Fragment>
+          )}
+         <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel>Building Type</InputLabel>
+              <Select
+                value={buildingType}
+                onChange={(e) => setBuildingType(e.target.value)}
+              >
+                <MenuItem value="Commercial">Commercial</MenuItem>
+                <MenuItem value="Residential">Residential</MenuItem>
+                <MenuItem value="Industrial">Industrial</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={hasSolar}
+                  onChange={(e) => setHasSolar(e.target.checked)}
+                />
+              }
+              label="Solar Panels"
             />
-          )}
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h6" gutterBottom>
-            Onsite Distributed Energy Resource
-          </Typography>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={distributedEnergySource.onsiteDER}
-                onChange={handleDistributedEnergySourceChange}
-                name="onsiteDER"
-              />
-            }
-            label="Onsite Distributed Energy Resource"
-          />
-          {distributedEnergySource.onsiteDER && (
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <TextField
-                  label="DES Type"
-                  fullWidth
-                  name="desType"
-                  value={distributedEnergySource.desType}
-                  onChange={handleDistributedEnergySourceChange}
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={hasBatteryStorage}
+                  onChange={(e) => setHasBatteryStorage(e.target.checked)}
                 />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  label="Capacity (kW)"
-                  fullWidth
-                  name="capacity"
-                  value={distributedEnergySource.capacity}
-                  onChange={handleDistributedEnergySourceChange}
+              }
+              label="Battery Storage"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={hasEVSE}
+                  onChange={(e) => setHasEVSE(e.target.checked)}
                 />
-              </Grid>
-            </Grid>
-          )}
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h6" gutterBottom>
-            Existing Electric Vehicle Charging Stations
-          </Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
+              }
+              label="EVSE"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Electrical Service Type"
+              value={electricalServiceType}
+              onChange={(e) => setElectricalServiceType(e.target.value)}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Available Circuits"
+              type="number"
+              value={availableCircuits}
+              onChange={(e) => setAvailableCircuits(e.target.value)}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Electrical Capacity"
+              type="number"
+              value={electricalCapacity}
+              onChange={(e) => setElectricalCapacity(e.target.value)}
+              fullWidth
+            />
+          </Grid>
+<          Grid item xs={12}>
               <TextField
-                label="Level"
-                fullWidth
-                name="level"
-                value={electricVehicleChargingStations.level}
-                onChange={handleElectricVehicleChargingStationsChange}
+              label="Electrical Capacity"
+              type="number"
+              value={transformerAndDistributionEquipment.electricalCapacity}
+              onChange={(e) => setTransformerAndDistributionEquipment({...transformerAndDistributionEquipment, electricalCapacity: e.target.value})}
+              fullWidth
               />
-            </Grid>
-            <Grid item xs={3}>
-              <TextField
-                label="Port Type"
-                fullWidth
-                name="portType"
-                value={electricVehicleChargingStations.portType}
-                onChange={handleElectricVehicleChargingStationsChange}
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TextField
-                label="Max Charging per Port"
-                fullWidth
-                name="maxChargingPerPort"
-                value={electricVehicleChargingStations.maxChargingPerPort}
-                onChange={handleElectricVehicleChargingStationsChange}
-              />
-            </Grid>
-            <Grid item xs={3}>
-              <TextField
-                label="Number"
-                fullWidth
-                name="number"
-                value={electricVehicleChargingStations.number}
-                onChange={handleElectricVehicleChargingStationsChange}
-              />
-            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Save
+            </Button>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" fullWidth>
-            SAVE
-          </Button>
-        </Grid>
-      </Grid>
-    </Grid>
+      </form>
+      <Box sx={{ mt: 2 }}>
+        <Typography>json-output:</Typography>
+        <Box id="json-output" sx={{ p: 2, border: '1px solid #ccc', borderRadius: 1 }}>
+        </Box>
+      </Box>
+    </Box>
   );
 }
+
+export default BuildingInfo;
