02090c000128dc84496f367263f2b969b566ffc6
RONGXIN YIN <rongxinyin@gmail.com>
2024-07-19 13:39:27 -0700

    add python travel routes

--- python-backend/scripts/process-empl-data.py
+++ python-backend/scripts/process-empl-data.py
@@ -1,9 +1,60 @@
 import sys
 import json
+import pandas as pd
+import numpy as np
+import googlemaps
+import urllib
+import simplejson
+import yaml
+import requests
+import random
+
+from datetime import datetime
+
+# Setup google maps config and credentials
+with open("../config.yaml", 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+maps_config = cfg.get('google', {})
+ryin_api = maps_config.get('api_key')
+travel_base_url = maps_config.get('url')
+gmaps = googlemaps.Client(key=ryin_api)
+
+def get_travel_distance(empl, dest):
+    # add employee geocode
+    empl_geocode = gmaps.geocode(empl['zip_code'])
+
+    try:
+        empl_loc = empl_geocode[0]['geometry']['location']
+        empl['geocode'] = "{},{}".format(empl_loc['lat'], empl_loc['lng'])
+    except:
+        print('No employee location found.')
+        empl['geocode'] = np.nan
+    # add dest geo location to each employee data
+    empl['dest'] = dest
+    # get travel distance between employee home and workplace
+    empl.update(getDistance(empl['geocode'], empl['dest'])['rows'][0]['elements'][0])
+
+    return empl
+
+def getDistance(origin, dest):
+
+    url = travel_base_url.format(origin, dest)+'&key={}'.format(ryin_api)
+    response = simplejson.load(urllib.request.urlopen(url))
+    # print(response)
+
+    return response
 
 def process_data(data):
-    # Replace this with your actual data processing logic
-    result = {"status": "success", "data": data}
+    # Note - no space between lat and lng
+    dest_geocode = "32.89204,-117.13096" # geocode - Miramar
+
+    # Create a list of json data for each vehicle and dump to json file
+    empl_commute = []
+    for empl in data:
+        empl_commute.append(get_travel_distance(empl, dest_geocode))
+    
+    # return the processed data
+    result = {"status": "success", "data": empl_commute}
     return result
 
 if __name__ == "__main__":
