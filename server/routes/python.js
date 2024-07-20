63970cf81a59d08c9f571fd9f763b58a43e27f34
Michael Leong <michaelleong1229@gmail.com>
2024-07-19 19:54:45 -0700

    rearranging files

--- server/routes/python.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { runPythonScript } = require("../controllers/pythonController");
-
-const router = express.Router();
-
-router.post("/process", runPythonScript);
-
-module.exports = router;
