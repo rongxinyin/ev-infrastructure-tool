2944c7721a2dd040dc5d11e45ef3eb02173bc1ea
leongmichael <michaelleong1229@gmail.com>
2024-08-12 07:51:25 -0700

    folder reorganization

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import { spawn } from "child_process";
 import pythonRoutes from "./routes/python.js";
 
 // app
